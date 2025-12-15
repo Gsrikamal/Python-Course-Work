0e119db9651b8b67693b4282ff3d58c645ddad9d
Gsrikamal <srikamalgummadi@gmail.com>
2025-12-15 18:09:41 +0530

    Added weekly test file

---
+++ Weekly Test/test1.py
@@ -0,0 +1,96 @@
+print("Hello World".lower())
+
+greeting = "good morning"
+print(greeting.upper())
+
+data = " python "
+print(data.strip())
+
+line = "Python is tough"
+print(line.replace("tough", "easy"))
+
+word = "banana"
+print(word.count("a"))
+
+text = "python is fun"
+print(text.split())
+
+sentence = "learn python"
+print(len(sentence))
+
+value = "test123"
+print(value.isalnum())
+
+text = "HELLO"
+print(text.lower().capitalize())
+
+email = "student@domain.com"
+print(email.endswith(".com"))
+
+numbers = [45, 67, 89, 32]
+numbers.sort()
+print(numbers)
+
+nums = [10, 20, 30]
+nums.append(40)
+print(nums)
+
+colors = ["red", "blue", "green"]
+colors.remove("blue")
+print(colors)
+
+values = [1, 2, 3, 2, 1]
+print(values.count(2))
+
+items = ["pen", "book", "pencil"]
+print(",".join(items))
+
+scores = [100, 90, 80]
+scores.reverse()
+print(scores)
+
+students = ("Anu", "Ravi", "Meena")
+print(students[1])
+
+details = ("ID101", "Sita", "CSE")
+print(list(details))
+
+data = (5, 3, 9, 1)
+print(max(data))
+
+group = ("A", "B", "C", "D")
+print(group[-2:])
+
+a = "10"
+b = "20"
+print(int(a) + int(b))
+
+num = 25
+print(str(num))
+
+x = 10
+y = 3
+print(x % y)
+
+a = 5
+b = 2
+print(a ** b)
+
+p = 15
+q = 20
+print(p < q)
+
+numbers = [1, 2, 3, 4, 5]
+print(sum(numbers))
+
+data = [3, 7, 1, 9]
+print(max(data))
+
+word = "mississippi"
+print(word.count("s"))
+
+text = "I love Python"
+print(text.replace("love", "learn"))
+
+values = [10, 20, 30, 40]
+print(tuple(values))
